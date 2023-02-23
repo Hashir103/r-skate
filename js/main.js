c0928c392be538fda9361c5c2051322005c23a80
Hashir <hashirsami@hotmail.ca>
2023-02-22 20:44:19 -0500

    fixed pages

--- js/main.js
+++ js/main.js
@@ -142,34 +142,6 @@ Created: Colorib
         horizrailenabled: false
     });
 
-    /*------------------
-        CountDown
-    --------------------*/
-    // For demo preview start
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
-
-    if(mm == 12) {
-        mm = '01';
-        yyyy = yyyy + 1;
-    } else {
-        mm = parseInt(mm) + 1;
-        mm = String(mm).padStart(2, '0');
-    }
-    var timerdate = mm + '/' + dd + '/' + yyyy;
-    // For demo preview end
-
-
-    // Uncomment below and use your date //
-
-    /* var timerdate = "2020/12/30" */
-
-	$("#countdown-time").countdown(timerdate, function(event) {
-        $(this).html(event.strftime("<div class='countdown__item'><span>%D</span> <p>Day</p> </div>" + "<div class='countdown__item'><span>%H</span> <p>Hour</p> </div>" + "<div class='countdown__item'><span>%M</span> <p>Min</p> </div>" + "<div class='countdown__item'><span>%S</span> <p>Sec</p> </div>"));
-    });
-
     /*-------------------
 		Range Slider
 	--------------------- */
